cb3be355f7be91c91d7fa768fc16723924bb73fa
LauraLaukagale <laura.zaze@gmail.com>
2021-07-02 11:24:21 -0700

    Initial commit

---
+++ src/HeroApplication.java
@@ -0,0 +1,55 @@
+public class HeroApplication {
+
+    public static void main(String[] args) {
+
+        // Variable initialization
+        String heroName;
+        String heroCity;
+
+        String heroPower1, heroPower2, heroPower3;
+
+        boolean isEvil;
+
+        double heroSalary;
+        String realName;
+
+        String universe;
+
+        // Variable assignment
+        heroName = "AliexpressMan";
+        heroCity = "Dobele";
+        heroPower1 = "Super discounts";
+        heroPower2 = "Fast Delivery";
+        heroPower3 = "Blank";
+        isEvil = false;
+        heroSalary = 99.99;
+        realName = "Artjoms";
+        universe = "Ours";
+
+        // Print out Hero information card
+        System.out.println("*****************");
+        System.out.println("*** Hero INFORMATION ***");
+        System.out.println("******");
+
+        System.out.println("Hero name: " + heroName);
+        System.out.println("This hero lives in " +heroCity);
+
+        System.out.println("--------");
+        System.out.println("Hero Superpowers:");
+        System.out.println("---" + heroPower1);
+        System.out.println("---" + heroPower2);
+        System.out.println("---" + heroPower3);
+
+        System.out.println("This super hero is evil? " + isEvil );
+        System.out.println("This hero earns " + heroSalary);
+        System.out.println("Real identity " + realName );
+        System.out.println("Hero lives in " + "Universe");
+
+        // Print out Hero information card
+        System.out.println("*****************");
+        System.out.println("*** Hero INFORMATION ***");
+        System.out.println("******");
+
+
+    }
+}
